8fa9e5a20e86e3a055480ab223fdebdaaadf5bc3
RamirezLina <linaramirezm11@gmail.com>
2023-06-05 19:52:16 -0500

    Desarrollo de los ejercico 15 al 19

---
+++ Guia7_Ejer/src/guia7_ejer/Ejer14_20.java
@@ -0,0 +1,233 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia7_ejer;
+import java.util.Scanner;
+/**
+ *
+ * @author LINA RAMIREZ
+ */
+public class Ejer14_20 {
+    
+    public static void main(String [] args){
+        Scanner read = new Scanner (System.in);
+        
+//        ejer15(args);
+//        ejer16 (args);
+//        ejer17 (args);
+//        ejer18(args);
+//        ejer19 (args);
+        ejer20 (args);
+        
+        
+        
+        
+    }
+    
+    public static void aleatorio(int[] vector){
+        Scanner read = new Scanner (System.in);
+                
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random()*10000);
+        }
+       
+    }
+    public static void mostrar(int[] vector){
+        System.out.print("[ ");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + " ");
+        }
+        System.out.println("]");
+    }
+    
+    public static void ejer15(String[] args){
+        Scanner read = new Scanner (System.in);
+        int n = 100;
+        int[] vector = new int [n];
+        
+        for (int i = 0; i < n; i++) {
+            vector [i] = i + 1;
+                              
+        }
+        mostrarDesc(vector, n);
+
+    }
+    
+    public static void mostrarDesc(int[] vector, int n){
+        System.out.print("[ ");
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(vector[i] + " ");
+        }
+        System.out.println("]");
+    }
+    
+    public static void ejer16(String[] args){
+        Scanner read = new Scanner (System.in);
+        System.out.println("Escriba el largo del vector");
+        int n = read.nextInt();
+        int[] vector = new int [n];
+        
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random()*10);
+            
+        }
+       
+        System.out.println("Indique el numero a buscar en el vector, valores del 0 al 10");
+        int num  = read.nextInt();
+        int rep = 0;
+        for (int i = 0; i < vector.length; i++) {
+            if (vector[i] == num) {
+                System.out.println("El numero se encuentra en la pos "+ i);
+                rep += 1;
+            }
+            
+        }
+        System.out.println("El numero se encontró repetido "+ rep +" veces");
+    }
+    
+    public static void ejer17(String[] args){
+        Scanner read = new Scanner (System.in);
+        System.out.println("Escriba el largo del vector");
+        int n = read.nextInt();
+        int[] vector = new int [n];
+        aleatorio(vector);
+        int Dig1 = 0; int Dig2 = 0; int Dig3 = 0; int Dig4 = 0; int Dig5 = 0;
+        
+        
+        for (int i = 0; i < vector.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                boolean divi = vector[i]/(Math.pow(10,j))<10;
+                if (divi) {
+                    switch (j) {
+                    case 0:
+                        Dig1 += 1;
+                        break;
+                    case 1:
+                        Dig2 += 1;
+                        break;
+                    case 2:
+                        Dig3 += 1;
+                        break;
+                    case 3:
+                        Dig4 += 1;
+                        break;
+                    case 4:
+                        Dig5 += 1;
+                        break;
+                    
+                    }
+                    break;
+                }
+                
+                
+                    
+            }
+        }
+        
+        mostrar(vector);
+        System.out.println("Se tiene "+Dig1+" numeros de un digito");
+        System.out.println("Se tiene "+Dig2+" numeros de dos digitos");
+        System.out.println("Se tiene "+Dig3+" numeros de tres digitos");
+        System.out.println("Se tiene "+Dig4+" numeros de cuatro digitos");
+        System.out.println("Se tiene "+Dig5+" numeros de cinco digitos");
+        
+    }  
+    
+    public static void ejer18(String[] args){
+        Scanner read = new Scanner (System.in);
+        int n = 4;
+        int  m = n;
+        int[][] matriz = new int [n][m];
+        int[][] matrizT = new int [n][m];
+        
+        llenarMat(matriz);
+        System.out.println("La matriz original es ");
+        mostrarMat(matriz);
+        transMat(matrizT, matriz);
+        System.out.println("");
+        System.out.println("La matriz transpuesta es");
+        mostrarMat(matrizT);
+        
+    }
+    
+    
+    public static void llenarMat(int[][] matriz) {
+    for (int i = 0; i < matriz.length; i++) {
+        for (int j = 0; j < matriz[i].length; j++) {
+            matriz[i][j] = (int) (Math.random() * 10);
+        }
+    }
+}
+
+    
+    public static void mostrarMat (int [][] matriz){
+        for(int [] fila : matriz) {
+            System.out.print("[ ");
+            for (int elemento : fila) {
+                System.out.print(elemento+" ");
+            }
+            System.out.println("]");
+        }
+    }
+        
+    public static void transMat (int [][] matrizT, int[][] matriz){
+        for (int i = 0; i < matrizT.length; i++) {
+            for (int j = 0; j < matrizT[0].length; j++) {
+                matrizT[i][j] = matriz[j][i];
+            }
+            
+        }
+    }
+    
+    public static void ejer19(String[] args){
+        Scanner read = new Scanner (System.in);
+        int[][] matriz = {{0,-2,4},{2,0,2},{-4,-2,0}};
+        int[][] matrizT = new int[matriz[0].length][matriz.length];
+//        int[][] matrizComp = new int[matriz[0].length][matriz.length];
+        System.out.println("La Matriz A es ");
+        mostrarMat(matriz);
+        transMatAnti(matrizT, matriz);
+        int [][] matrizComp ={{0,2,4},{2,0,2},{4,-2,0}};
+        System.out.println("");
+        System.out.println("La matriz B es ");
+        mostrarMat(matrizComp);
+        siMatAnti(matrizT, matrizComp);
+        
+    }
+    
+    public static void transMatAnti (int [][] matrizT, int[][] matriz){
+        for (int i = 0; i < matrizT.length; i++) {
+            for (int j = 0; j < matrizT[0].length; j++) {
+                matrizT[i][j] = -1*matriz[j][i];
+            }
+            
+        }
+    }
+    
+    public static void siMatAnti (int [][] matrizT, int[][] matrizComp){
+        int prueba = 1;
+        for (int i = 0; i < matrizT.length; i++) {
+            for (int j = 0; j < matrizT[0].length; j++) {
+                if (matrizT[i][j] != matrizComp[i][j]) {
+                    prueba = 0;
+                    break;
+                }
+            }
+        }
+        
+        if (prueba == 0) {
+            System.out.println("Las matriz B no es anti simétrica de la matriz original");
+  
+        }else{
+            System.out.println("La matriz B es anti simétrica de la matriz B");
+            
+        }
+    }
+    
+    public static void ejer20(String[] args){
+        Scanner read = new Scanner (System.in);
+        /*Mirar el EJERCICIO 6 De ejercicio 22 de PsEInt*/
+    }
+}
