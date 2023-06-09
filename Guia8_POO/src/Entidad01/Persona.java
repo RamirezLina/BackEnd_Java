6f16ef1732cfc0fa0274fe976f8f24f60ce6e4a7
RamirezLina <linaramirezm11@gmail.com>
2023-06-08 20:03:15 -0500

    Se añade carpeta de la guia 8 - Intro POO

---
+++ Guia8_POO/src/Entidad01/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad01;
+
+/**
+ *
+ * @author LINA RAMIREZ
+ */
+public class Persona {
+    public String nombre;
+    public int edad;
+    public String Genero;
+}
