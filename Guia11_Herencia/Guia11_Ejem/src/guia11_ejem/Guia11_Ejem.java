9800e6aa0c6b0eb65124c1634b423499ce2d1eb4
RamirezLina <linaramirezm11@gmail.com>
2023-08-24 19:59:01 -0500

    Guia 11 ejemplo 1

---
+++ Guia11_Herencia/Guia11_Ejem/src/guia11_ejem/Guia11_Ejem.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia11_ejem;
+
+import Entities.Animal;
+import Entities.Gato;
+import Entities.Perro;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LINA RAMIREZ
+ */
+public class Guia11_Ejem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Animal> animales = new ArrayList();
+        Animal a = new Animal();
+        Animal b = new Perro();
+        Animal c = new Gato();
+        
+        animales.add(a);
+        animales.add(b);
+        animales.add(c);
+        
+        for (Animal an : animales) {
+            an.hacerRuido();
+        }
+    }
+    
+}
